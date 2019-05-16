d9f91a73972e79546d106306abdbcb9b01eefc8a
RikVerheij <rik.verheij@gc.graafschapcollege.nl>
2019-05-16 15:20:01 +0200

    database

--- Database/Config.php
+++ Database/Config.php
@@ -4,7 +4,7 @@
 defined("DB_SERVER") ? null : define("DB_SERVER", "localhost");
 defined('DB_USER') ? null : define("DB_USER", "root");
 defined('DB_PASS') ? null : define("DB_PASS", "");
-defined('DB_NAME') ? null : define("DB_NAME", "scrumphp");
+defined('DB_NAME') ? null : define("DB_NAME", "case chain");
 
 $db = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 ?>
