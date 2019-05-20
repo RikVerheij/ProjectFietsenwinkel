8927669c4bf55c1a8130b99cf1f4de2ba0f2eea6
RikVerheij <rik.verheij@gc.graafschapcollege.nl>
2019-05-20 13:11:27 +0200

    css link

--- login.php
+++ login.php
@@ -29,6 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="Css/style.css";
 </head>
 <body>
 <div>
