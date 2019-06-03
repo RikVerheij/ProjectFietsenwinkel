f0fd392a4004b5303f6a2c1b60719866aef38519
RikVerheij <rik.verheij@gc.graafschapcollege.nl>
2019-06-03 13:36:15 +0200

    detail pagina blob

---
+++ footer.php
@@ -0,0 +1,114 @@
+<div class="ps-subscribe">
+    <div class="ps-container">
+        <div class="row">
+            <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 ">
+                <h3><i class="fa fa-envelope"></i>Meld aan voor de nieuwsbrief</h3>
+            </div>
+            <div class="col-lg-5 col-md-7 col-sm-12 col-xs-12 ">
+                <form class="ps-subscribe__form" action="do_action" method="post">
+                    <input class="form-control" type="text" placeholder="">
+                    <button>Meld nu aan</button>
+                </form>
+            </div>
+            <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 ">
+                <p>...en krijg een <span>€20</span> cadeaubon voor uw eerste aankoop.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="ps-footer bg--cover" data-background="images/background/parallax.jpg">
+    <div class="ps-footer__content">
+        <div class="ps-container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+                    <aside class="ps-widget--footer ps-widget--info">
+                        <header><a class="ps-logo" href="index.php"><img src="images/logo.png" alt=""></a>
+                            <h3 class="ps-widget__title">Adress Kantoor 1</h3>
+                        </header>
+                        <footer>
+                            <p><strong>460 West 34th Street, 15th floor, New York</strong></p>
+                            <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
+                            <p>Phone: +323 32434 5334</p>
+                            <p>Fax: ++323 32434 5333</p>
+                        </footer>
+                    </aside>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+                    <aside class="ps-widget--footer ps-widget--info second">
+                        <header>
+                            <h3 class="ps-widget__title">Adress Kantoor 2</h3>
+                        </header>
+                        <footer>
+                            <p><strong>PO Box 16122 Collins Victoria 3000 Australia</strong></p>
+                            <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
+                            <p>Phone: +323 32434 5334</p>
+                            <p>Fax: ++323 32434 5333</p>
+                        </footer>
+                    </aside>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
+                    <aside class="ps-widget--footer ps-widget--link">
+                        <header>
+                            <h3 class="ps-widget__title">Vind onze winkels</h3>
+                        </header>
+                        <footer>
+                            <ul class="ps-list--link">
+                                <li><a href="#">Kortingscode Code</a></li>
+                                <li><a href="#">Meld Aan Voor Email</a></li>
+                                <li><a href="#">Site Feedback</a></li>
+                                <li><a href="#">Banen</a></li>
+                            </ul>
+                        </footer>
+                    </aside>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
+                    <aside class="ps-widget--footer ps-widget--link">
+                        <header>
+                            <h3 class="ps-widget__title">Hulp nodig?</h3>
+                        </header>
+                        <footer>
+                            <ul class="ps-list--line">
+                                <li><a href="#">Bestelling Status</a></li>
+                                <li><a href="#">Verzending en Aflevering</a></li>
+                                <li><a href="#">Terugsturen</a></li>
+                                <li><a href="#">Betalingsmogelijkheden</a></li>
+                                <li><a href="#">Contact ons</a></li>
+                            </ul>
+                        </footer>
+                    </aside>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
+                    <aside class="ps-widget--footer ps-widget--link">
+                        <header>
+                            <h3 class="ps-widget__title">Fietsen</h3>
+                        </header>
+                        <footer>
+                            <ul class="ps-list--line">
+                                <li><a href="#">Mannenfietsen</a></li>
+                                <li><a href="#">Vrouwenfietsen</a></li>
+                                <li><a href="#">E-Bikes</a></li>
+                                <li><a href="#">Overige fietsen</a></li>
+                            </ul>
+                        </footer>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ps-footer__copyright">
+        <div class="ps-container">
+            <div class="row">
+
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                    <ul class="ps-social">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</main>
--- product-detail.php
+++ product-detail.php
@@ -1,3 +1,10 @@
+<?php
+include("Database/Config.php");
+$sql = "SELECT * FROM product";
+$sth = $db->query($sql);
+$result=mysqli_fetch_array($sth);
+?>
+
 <!DOCTYPE html>
 <!--[if IE 7]><html class="ie ie7"><![endif]-->
 <!--[if IE 8]><html class="ie ie8"><![endif]-->
@@ -20,198 +27,27 @@
     <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="plugins/ps-icon/style.css">
     <!-- CSS Library-->
-    <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="plugins/owl-carousel/assets/owl.carousel.css">
-    <link rel="stylesheet" href="plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css">
-    <link rel="stylesheet" href="plugins/slick/slick/slick.css">
-    <link rel="stylesheet" href="plugins/bootstrap-select/dist/css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="plugins/Magnific-Popup/dist/magnific-popup.css">
-    <link rel="stylesheet" href="plugins/jquery-ui/jquery-ui.min.css">
-    <link rel="stylesheet" href="plugins/revolution/css/settings.css">
-    <link rel="stylesheet" href="plugins/revolution/css/layers.css">
-    <link rel="stylesheet" href="plugins/revolution/css/navigation.css">
-    <!-- Custom-->
-    <link rel="stylesheet" href="css/style.css">
-    <!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
-    <!--WARNING: Respond.js doesn't work if you view the page via file://-->
-    <!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+      <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="plugins/owl-carousel/assets/owl.carousel.css">
+      <link rel="stylesheet" href="plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css">
+      <link rel="stylesheet" href="plugins/slick/slick/slick.css">
+      <link rel="stylesheet" href="plugins/bootstrap-select/dist/css/bootstrap-select.min.css">
+      <link rel="stylesheet" href="plugins/Magnific-Popup/dist/magnific-popup.css">
+      <link rel="stylesheet" href="plugins/jquery-ui/jquery-ui.min.css">
+      <link rel="stylesheet" href="plugins/revolution/css/settings.css">
+      <link rel="stylesheet" href="plugins/revolution/css/layers.css">
+      <link rel="stylesheet" href="plugins/revolution/css/navigation.css">
+      <!-- Custom-->
+      <link rel="stylesheet" href="css/style.css">
+      <!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
+      <!--WARNING: Respond.js doesn't work if you view the page via file://-->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
   </head>
-  <!--[if IE 7]><body class="ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
-  <!--[if IE 8]><body class="ie8 lt-ie9 lt-ie10"><![endif]-->
-  <!--[if IE 9]><body class="ie9 lt-ie10"><![endif]-->
-  <body class="ps-loading">
-    <div class="header--sidebar"></div>
-    <header class="header">
-      <div class="header__top">
-        <div class="container-fluid">
-          <div class="row">
-                <div class="col-lg-6 col-md-8 col-sm-6 col-xs-12 ">
-                  <p>460 West 34th Street, 15th floor, New York  -  Hotline: 804-377-3580 - 804-399-3580</p>
-                </div>
-                <div class="col-lg-6 col-md-4 col-sm-6 col-xs-12 ">
-                  <div class="header__actions"><a href="login.php">Login & Regiser</a>
-                    <div class="btn-group ps-dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">USD<i class="fa fa-angle-down"></i></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#"><img src="images/flag/usa.svg" alt=""> USD</a></li>
-                        <li><a href="#"><img src="images/flag/singapore.svg" alt=""> SGD</a></li>
-                        <li><a href="#"><img src="images/flag/japan.svg" alt=""> JPN</a></li>
-                      </ul>
-                    </div>
-                    <div class="btn-group ps-dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language<i class="fa fa-angle-down"></i></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">English</a></li>
-                        <li><a href="#">Japanese</a></li>
-                        <li><a href="#">Chinese</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-          </div>
-        </div>
-      </div>
-      <nav class="navigation">
-        <div class="container-fluid">
-          <div class="navigation__column left">
-            <div class="header__logo"><a class="ps-logo" href="index.php"><img src="images/logo.png" alt=""></a></div>
-          </div>
-          <div class="navigation__column center">
-                <ul class="main-menu menu">
-                  <li class="menu-item menu-item-has-children dropdown"><a href="index.php">Home</a>
-                        <ul class="sub-menu">
-                          <li class="menu-item"><a href="index.php">Homepage #1</a></li>
-                          <li class="menu-item"><a href="#">Homepage #2</a></li>
-                          <li class="menu-item"><a href="#">Homepage #3</a></li>
-                        </ul>
-                  </li>
-                  <li class="menu-item menu-item-has-children has-mega-menu"><a href="#">Men</a>
-                    <div class="mega-menu">
-                      <div class="mega-wrap">
-                        <div class="mega-column">
-                          <ul class="mega-item mega-features">
-                            <li><a href="product-listing.php">NEW RELEASES</a></li>
-                            <li><a href="product-listing.php">FEATURES SHOES</a></li>
-                            <li><a href="product-listing.php">BEST SELLERS</a></li>
-                            <li><a href="product-listing.php">NOW TRENDING</a></li>
-                            <li><a href="product-listing.php">SUMMER ESSENTIALS</a></li>
-                            <li><a href="product-listing.php">MOTHER'S DAY COLLECTION</a></li>
-                            <li><a href="product-listing.php">FAN GEAR</a></li>
-                          </ul>
-                        </div>
-                        <div class="mega-column">
-                          <h4 class="mega-heading">Shoes</h4>
-                          <ul class="mega-item">
-                            <li><a href="product-listing.php">All Shoes</a></li>
-                            <li><a href="product-listing.php">Running</a></li>
-                            <li><a href="product-listing.php">Training & Gym</a></li>
-                            <li><a href="product-listing.php">Basketball</a></li>
-                            <li><a href="product-listing.php">Football</a></li>
-                            <li><a href="product-listing.php">Soccer</a></li>
-                            <li><a href="product-listing.php">Baseball</a></li>
-                          </ul>
-                        </div>
-                        <div class="mega-column">
-                          <h4 class="mega-heading">CLOTHING</h4>
-                          <ul class="mega-item">
-                            <li><a href="product-listing.php">Compression & Nike Pro</a></li>
-                            <li><a href="product-listing.php">Tops & T-Shirts</a></li>
-                            <li><a href="product-listing.php">Polos</a></li>
-                            <li><a href="product-listing.php">Hoodies & Sweatshirts</a></li>
-                            <li><a href="product-listing.php">Jackets & Vests</a></li>
-                            <li><a href="product-listing.php">Pants & Tights</a></li>
-                            <li><a href="product-listing.php">Shorts</a></li>
-                          </ul>
-                        </div>
-                        <div class="mega-column">
-                          <h4 class="mega-heading">Accessories</h4>
-                          <ul class="mega-item">
-                            <li><a href="product-listing.php">Compression & Nike Pro</a></li>
-                            <li><a href="product-listing.php">Tops & T-Shirts</a></li>
-                            <li><a href="product-listing.php">Polos</a></li>
-                            <li><a href="product-listing.php">Hoodies & Sweatshirts</a></li>
-                            <li><a href="product-listing.php">Jackets & Vests</a></li>
-                            <li><a href="product-listing.php">Pants & Tights</a></li>
-                            <li><a href="product-listing.php">Shorts</a></li>
-                          </ul>
-                        </div>
-                        <div class="mega-column">
-                          <h4 class="mega-heading">BRAND</h4>
-                          <ul class="mega-item">
-                            <li><a href="product-listing.php">NIKE</a></li>
-                            <li><a href="product-listing.php">Adidas</a></li>
-                            <li><a href="product-listing.php">Dior</a></li>
-                            <li><a href="product-listing.php">B&G</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li class="menu-item"><a href="#">Women</a></li>
-                  <li class="menu-item"><a href="#">Kids</a></li>
-                  <li class="menu-item menu-item-has-children dropdown"><a href="#">News</a>
-                        <ul class="sub-menu">
-                          <li class="menu-item menu-item-has-children dropdown"><a href="blog-grid.php">Blog-grid</a>
-                                <ul class="sub-menu">
-                                  <li class="menu-item"><a href="blog-grid.php">Blog Grid 1</a></li>
-                                  <li class="menu-item"><a href="blog-grid-2.php">Blog Grid 2</a></li>
-                                </ul>
-                          </li>
-                          <li class="menu-item"><a href="blog-list.php">Blog List</a></li>
-                        </ul>
-                  </li>
-                  <li class="menu-item menu-item-has-children dropdown"><a href="#">Contact</a>
-                        <ul class="sub-menu">
-                          <li class="menu-item"><a href="contact-us.php">Contact Us #1</a></li>
-                          <li class="menu-item"><a href="contact-us.php">Contact Us #2</a></li>
-                        </ul>
-                  </li>
-                </ul>
-          </div>
-          <div class="navigation__column right">
-            <form class="ps-search--header" action="do_action" method="post">
-              <input class="form-control" type="text" placeholder="Search Product…">
-              <button><i class="ps-icon-search"></i></button>
-            </form>
-            <div class="ps-cart"><a class="ps-cart__toggle" href="#"><span><i>20</i></span><i class="ps-icon-shopping-cart"></i></a>
-              <div class="ps-cart__listing">
-                <div class="ps-cart__content">
-                  <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
-                    <div class="ps-cart-item__thumbnail"><a href="product-detail.php"></a><img src="images/cart-preview/1.jpg" alt=""></div>
-                    <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="product-detail.php">Amazin’ Glazin’</a>
-                      <p><span>Quantity:<i>12</i></span><span>Total:<i>£176</i></span></p>
-                    </div>
-                  </div>
-                  <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
-                    <div class="ps-cart-item__thumbnail"><a href="product-detail.php"></a><img src="images/cart-preview/2.jpg" alt=""></div>
-                    <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="product-detail.php">The Crusty Croissant</a>
-                      <p><span>Quantity:<i>12</i></span><span>Total:<i>£176</i></span></p>
-                    </div>
-                  </div>
-                  <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
-                    <div class="ps-cart-item__thumbnail"><a href="product-detail.php"></a><img src="images/cart-preview/3.jpg" alt=""></div>
-                    <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="product-detail.php">The Rolling Pin</a>
-                      <p><span>Quantity:<i>12</i></span><span>Total:<i>£176</i></span></p>
-                    </div>
-                  </div>
-                </div>
-                <div class="ps-cart__total">
-                  <p>Number of items:<span>36</span></p>
-                  <p>Item Total:<span>£528.00</span></p>
-                </div>
-                <div class="ps-cart__footer"><a class="ps-btn" href="cart.php">Check out<i class="ps-icon-arrow-left"></i></a></div>
-              </div>
-            </div>
-            <div class="menu-toggle"><span></span></div>
-          </div>
-        </div>
-      </nav>
-    </header>
-    <div class="header-services">
-      <div class="ps-services owl-slider" data-owl-auto="true" data-owl-loop="true" data-owl-speed="7000" data-owl-gap="0" data-owl-nav="true" data-owl-dots="false" data-owl-item="1" data-owl-item-xs="1" data-owl-item-sm="1" data-owl-item-md="1" data-owl-item-lg="1" data-owl-duration="1000" data-owl-mousedrag="on">
-        <p class="ps-service"><i class="ps-icon-delivery"></i><strong>Free delivery</strong>: Get free standard delivery on every order with Sky Store</p>
-        <p class="ps-service"><i class="ps-icon-delivery"></i><strong>Free delivery</strong>: Get free standard delivery on every order with Sky Store</p>
-        <p class="ps-service"><i class="ps-icon-delivery"></i><strong>Free delivery</strong>: Get free standard delivery on every order with Sky Store</p>
-      </div>
-    </div>
+  <?php include 'header.php'
+
+  ?>
     <main class="ps-main">
       <div class="test">
         <div class="container">
@@ -228,15 +64,15 @@
               <div class="ps-product__thumbnail">
                 <div class="ps-product__preview">
                   <div class="ps-product__variants">
-                    <div class="item"><img src="images/shoe-detail/1.jpg" alt=""></div>
+                    <div class="item"><?php echo '<img class="image" src="data:image/jpeg;base64,'.base64_encode( $result['photo'] ).'"/>'; ?></div>
                     <div class="item"><img src="images/shoe-detail/2.jpg" alt=""></div>
                     <div class="item"><img src="images/shoe-detail/3.jpg" alt=""></div>
                     <div class="item"><img src="images/shoe-detail/3.jpg" alt=""></div>
                     <div class="item"><img src="images/shoe-detail/3.jpg" alt=""></div>
-                  </div><a class="popup-youtube ps-product__video" href="http://www.youtube.com/watch?v=0O2aH4XLbto"><img src="images/shoe-detail/1.jpg" alt=""><i class="fa fa-play"></i></a>
+                  </div><a class="popup-youtube ps-product__video" href="https://www.youtube.com/watch?v=94hwZMZe83c"><img src="images/shoe-detail/1.jpg" alt=""><i class="fa fa-play"></i></a>
                 </div>
                 <div class="ps-product__image">
-                  <div class="item"><img class="zoom" src="images/shoe-detail/1.jpg" alt="" data-zoom-image="images/shoe-detail/1.jpg"></div>
+                  <div class="item"><?php echo '<img class="image" src="data:image/jpeg;base64,'.base64_encode( $result['photo'] ).'" />'; ?></div>
                   <div class="item"><img class="zoom" src="images/shoe-detail/2.jpg" alt="" data-zoom-image="images/shoe-detail/2.jpg"></div>
                   <div class="item"><img class="zoom" src="images/shoe-detail/3.jpg" alt="" data-zoom-image="images/shoe-detail/3.jpg"></div>
                 </div>
