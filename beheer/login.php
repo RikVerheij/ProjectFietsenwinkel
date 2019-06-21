2ff81dae536e806e52cbfbce203932c638f09dfd
Drøyy0412 <Mikevanbatenburg@outlook.com>
2019-06-21 10:12:01 +0200

    admin + sorting

---
+++ beheer/login.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<!--[if IE 7]><html class="ie ie7"><![endif]-->
+<!--[if IE 8]><html class="ie ie8"><![endif]-->
+<!--[if IE 9]><html class="ie ie9"><![endif]-->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="../apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="../favicon-bike.png" rel="icon">
+    <meta name="author" content="Nghia Minh Luong">
+    <meta name="keywords" content="Default Description">
+    <meta name="description" content="Default keyword">
+    <title>Chain gang - Login</title>
+    <!-- Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:300,400,700%7CMontserrat:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="../plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../plugins/ps-icon/style.css">
+    <!-- CSS Library-->
+    <link rel="stylesheet" href="../plugins/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../plugins/owl-carousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="../plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css">
+    <link rel="stylesheet" href="../plugins/slick/slick/slick.css">
+    <link rel="stylesheet" href="../plugins/bootstrap-select/dist/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="../plugins/Magnific-Popup/dist/magnific-popup.css">
+    <link rel="stylesheet" href="../plugins/jquery-ui/jquery-ui.min.css">
+    <link rel="stylesheet" href="../plugins/revolution/css/settings.css">
+    <link rel="stylesheet" href="../plugins/revolution/css/layers.css">
+    <link rel="stylesheet" href="../plugins/revolution/css/navigation.css">
+    <!-- Custom-->
+    <link rel="stylesheet" href="../Css/style.css">
+
+    <?php
+    include("../Database/Config.php");
+
+    session_start();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username and password sent from form
+
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        $sql = "SELECT customer_id FROM customer WHERE username = '$username' and password = '$password'";
+        $result = mysqli_query($db, $sql);
+
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $active = $row['active'];
+
+        $count = mysqli_num_rows($result);
+
+        if ($count == 1) {
+            $_SESSION['login_user'] = $username;
+            header("location: index.php");
+        } else {
+            $error = "Your Login Name or Password is invalid";
+        }
+    }
+    include 'header.php'
+    ?>
+    <html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+              integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="../Css/style.css">
+    </head>
+<body>
+<main class="ps-main">
+    <div class="ps-banner">
+        <div class="d-flex">
+
+        </div>
+
+        <!--        <!--Google map-->
+        <!--        <div id="map-container-google-2" class="z-depth-1-half map-container" style="height: 500px">-->
+        <!--            <iframe src="https://maps.google.com/maps?q=chicago&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"-->
+        <!--                    style="border:0" allowfullscreen></iframe>-->
+        <!--        </div>-->
+        <!---->
+        <!--        <!--Google Maps-->
+
+
+        <!-- JS Library-->
+        <script type="text/javascript" src="../plugins/jquery/dist/jquery.min.js"></script>
+        <script type="text/javascript" src="../plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../plugins/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
+        <script type="text/javascript" src="../plugins/owl-carousel/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="../plugins/gmap3.min.js"></script>
+        <script type="text/javascript" src="../plugins/imagesloaded.pkgd.js"></script>
+        <script type="text/javascript" src="../plugins/isotope.pkgd.min.js"></script>
+        <script type="text/javascript" src="../plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+        <script type="text/javascript" src="../plugins/jquery.matchHeight-min.js"></script>
+        <script type="text/javascript" src="../plugins/slick/slick/slick.min.js"></script>
+        <script type="text/javascript" src="../plugins/elevatezoom/jquery.elevatezoom.js"></script>
+        <script type="text/javascript" src="../plugins/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="../plugins/jquery-ui/jquery-ui.min.js"></script>
+        <script type="text/javascript"
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAx39JFH5nhxze1ZydH-Kl8xXM3OK4fvcg&amp;region=GB"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript"
+                src="../plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+        <script type="text/javascript" src="../plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <!-- Custom scripts-->
+        <script type="text/javascript" src="../js/main.js"></script>
+
+        <div class="ps-login">
+            <div class="ps-container">
+                <div class="row">
+                    <div class="mx-auto">
+                        <form action="" method="POST" class="post-border">
+                            <table class="form-width">
+                                <tr>
+                                    <div class="form-group">
+                                        <td><input type="text" class="form-control" name="username" id="Gebruikersnaam" placeholder="Gebruikersnaam"></td>
+                                    </div>
+                                </tr>
+                                <tr>
+                                    <div class="form-group">
+                                        <td><input type="password" class="form-control" name="password" id="Wachtwoord" placeholder="Wachtwoord"></td>
+                                    </div>
+                                </tr>
+                                <tr>
+                                    <td><input type="submit" class="ps-btn ps-btn--sm ps-beheer-btn" value=" Login "/>
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        include 'footer.php'
+        ?>
+</body>
+</html>
